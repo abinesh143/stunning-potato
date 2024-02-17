760b36f09a2c5fc9a9c845ed64ab47f7bbf23273
abinesh143 <abi.posbyz@gmail.com>
2024-02-18 00:33:44 +0530

    loader

--- src/components/BuildRequest.jsx
+++ src/components/BuildRequest.jsx
@@ -1,13 +1,16 @@
 import { useCallback, useEffect, useState } from "react";
+import Loader from "./Loader";
 
 const BuildRequest = () => {
   const [build, setBuild] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchResult = useCallback(async (quesId) => {
     const response = await fetch("/api/publish", {
       method: "GET",
     });
     const publish = await response.json();
+    setLoading(false);
     setBuild(publish);
   }, []);
 
@@ -27,43 +30,49 @@ const BuildRequest = () => {
             <span className="font-semibold">18:26 ⏰</span>
           </p>
         </div>
-        <div className="grid grid-cols-3 gap-6">
-          {build.map((b, index) => (
-            <div
-              key={`build-${index}`}
-              class=" p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-            >
-              <h5 class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Andriod - {b.andriodStatus} - {b.andriodReq ? "Want" : "None"}
-              </h5>
-              <h5 class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Ios - {b.iosStatus} - {b.iosReq ? "Want" : "None"}
-              </h5>
-
-              <a
-                href="#"
-                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        {loading ? (
+          <div className="flex justify-center items-center h-96">
+            <Loader />
+          </div>
+        ) : (
+          <div className="grid grid-cols-3 gap-6">
+            {build.map((b, index) => (
+              <div
+                key={`build-${index}`}
+                class=" p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
               >
-                Read more
-                <svg
-                  class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
+                <h5 class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Andriod - {b.andriodStatus} - {b.andriodReq ? "Want" : "None"}
+                </h5>
+                <h5 class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Ios - {b.iosStatus} - {b.iosReq ? "Want" : "None"}
+                </h5>
+
+                <a
+                  href="#"
+                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </a>
-            </div>
-          ))}
-        </div>
+                  Read more
+                  <svg
+                    class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </main>
   );
--- src/components/Feeback.jsx
+++ src/components/Feeback.jsx
@@ -1,15 +1,17 @@
 import { useCallback, useEffect, useState } from "react";
+import Loader from "./Loader";
 
 const Feedback = () => {
   const [feedback, setFeedback] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchResult = useCallback(async () => {
     const response = await fetch("/api/feedback", {
       method: "GET",
     });
     const feed = await response.json();
-    console.log(feed, 'feed')
     setFeedback(feed);
+    setLoading(false);
   }, []);
 
   useEffect(() => {
@@ -22,46 +24,55 @@ const Feedback = () => {
           <div className="sm:text-xl xl:text-2xl font-semibold capitalize ">
             Feedback Details
           </div>
-         
+
           <p className="text-sm sm:text-lg xl:text-xl">
             06/02/2024,
             <span className="font-semibold">18:26 ⏰</span>
           </p>
         </div>
-        <div className="grid grid-cols-3 gap-6">
-          {feedback.map((feed, index) => (
-            <div key={`feedback-${index}`} class="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {feed.fullName} - {feed.userEmail}
-              </h5>
-              <div className="my-2 text-lg">{feed.subject}</div>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {feed.description}
-              </p>
-              <a
-                href="#"
-                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        {loading ? (
+          <div className="flex justify-center items-center h-96">
+            <Loader />
+          </div>
+        ) : (
+          <div className="grid grid-cols-3 gap-6">
+            {feedback.map((feed, index) => (
+              <div
+                key={`feedback-${index}`}
+                class="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
               >
-                Done
-                <svg
-                  class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {feed.fullName} - {feed.userEmail}
+                </h5>
+                <div className="my-2 text-lg">{feed.subject}</div>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {feed.description}
+                </p>
+                <a
+                  href="#"
+                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </a>
-            </div>
-          ))}
-        </div>
+                  Done
+                  <svg
+                    class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </main>
   );
