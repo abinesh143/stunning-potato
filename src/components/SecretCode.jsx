bf187f2e72518ac1497473c45076866970a44ec9
abinesh143 <abi.posbyz@gmail.com>
2024-02-20 22:22:25 +0530

    a small change

--- src/components/SecretCode.jsx
+++ src/components/SecretCode.jsx
@@ -65,6 +65,7 @@ const SecretCode = () => {
                 id="andriod_apk"
                 className="bg-white border border-gray-300 text-gray-900 text-sm sm:text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Andriod Download Link"
+                value={link}
                 onChange={(e) => setLink(e.target.value)}
               />
             </div>
